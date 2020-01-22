eb777f78744e35d0d10d06770a0ec9ddcfb77a8d
zacharyinserra <zachary@christiansteven.com>
2020-01-22 16:55:07 -0500

    Added overview page and sign up page
    No css really

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function openLogin(){
+    document.getElementById("login").style.display = "block";
+}
+
+function closeLogin() {
+    document.getElementById("login").style.display = "none";
+}
+
+function pgSignUp() {
+    window.location.href = "../html/signup.html";
+}
+
+function pgIndex() {
+    window.location.href = "../html/index.html";
+}
+
+function pgOverview() {
+    window.location.href = "../html/overview.html"
+}
