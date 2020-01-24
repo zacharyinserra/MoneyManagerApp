09dc2b48c0c44f2cceb0a1879dafdc137354c37f
zacharyinserra <zachary@christiansteven.com>
2020-01-24 16:02:47 -0500

    more work and css

--- js/index.js
+++ js/index.js
@@ -18,13 +18,38 @@ function pgOverview() {
     window.location.href = "../html/overview.html"
 }
 
+// Shows the inputs to add a subscription after all the other listed subscriptions
 function showAddSub() {
+    $('#addSub').insertAfter("#newSubList");
     $('#addSub').show();
 }
 
-function addListItems(newItems) {
-    $('#subadubdub').append("<ul id='newList'></ul>");
+// Shows the inputs to add an account after all the other listed subscriptions
+function showAddAcc() {
+    $('#addAcc').insertAfter("#newAccList");
+    $('#addAcc').show();
+}
+
+// Adds user-input to a list in subscriptions
+function addSubItems(newItems) {
+    // Need to add check to make sure all inputs are filled out
+    $('#subadubdub').append("<ul id='newSubList'></ul>");
+    $('#newSubList').append("<li></li>");
     for (cnt = 0; cnt < newItems.length; cnt++) {
-        $('#newList').append("<li>" + newItems[cnt] + "</li>");
+        $('#newSubList').append("<li>" + newItems[cnt] + "</li>");
     }
+    $('#subTotals').insertAfter("#newSubList");
+    $('#addSub').hide();
+}
+
+// Adds user-input to a list in accounts
+function addAccItems(newItems) {
+    // Need to add check to make sure all inputs are filled out
+    $('#accountalicious').append("<ul id='newAccList'></ul>");
+    $('#newAccList').append("<li></li>");
+    for (cnt = 0; cnt < newItems.length; cnt++) {
+        $('#newAccList').append("<li>" + newItems[cnt] + "</li>");
+    }
+    $('#accTotals').insertAfter("#newAccList");
+    $('#addAcc').hide();
 }
